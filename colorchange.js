b44578bc964fea672c4071eb235c70218ae68cee
kjha4951 <128771901+kjha4951@users.noreply.github.com>
2023-08-02 12:25:27 +0530

    Add files via upload

--- colorchange.js
+++ colorchange.js
@@ -2,22 +2,33 @@ import React, { useEffect, useState } from 'react'
 
 function Color(){
     const [color,setColor]=useState('blue')
+    const [click,setColorclick]=useState('green')
+    
 
-    const changecolor=color=>{
-           setColor(color)
-    }
+    const changecolor=()=>{
+        setColor(color ==='blue'?'yellow':'blue')
+       }
+
     useEffect(()=>{
      document.body.style.backgroundColor=color
     },[color])
 
-  
+    function change(){
+        // setColorclick(!click)
+       setColorclick(click === 'green'?'red':'green')
+    }
     
 
      return(
         <div className='div1'>
             <button onClick={
                 ()=>{changecolor('yellow')}
-            }>Change color</button>
+            }   > Change color</button>
+
+            <button onClick={change} style={{backgroundColor: click }}>click on me to see the color</button>
+
+
+            
         </div>
      )
 }
